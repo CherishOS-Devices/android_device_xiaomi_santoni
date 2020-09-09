201a198ba11cb5b40169e26c393fd598772318d3
Khương Hùng <46420538+khuonghung1423@users.noreply.github.com>
2020-09-09 13:25:03 +0700

    CherishOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_santoni.mk
-
-COMMON_LUNCH_CHOICES := \
-    havoc_santoni-userdebug \
-    havoc_santoni-user \
-    havoc_santoni-eng
+    $(LOCAL_DIR)/cherish_santoni.mk
